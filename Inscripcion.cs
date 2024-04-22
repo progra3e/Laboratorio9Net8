3b4e4a321cc1bcba201ae8dd592d568bffd994b7
gramz <gramz@LAB4-CAT4>
2024-04-22 08:19:32 -0600

    Agregar archivos de proyecto.

---
+++ Inscripcion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio9Net8
+{
+    public class Inscripcion
+    {
+        public string Codigo { get; set; }
+        public string Dpi { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
---
+++ Taller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio9Net8
+{
+    public class Taller
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
